a961ed7ccb398c173f92883a39d408b69211acd3
Aleksei <boubnov28@gmail.com>
2025-11-27 20:55:25 -0600

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+import { defineConfig } from "vite";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
+    },
+  },
+  server: {
+    open: true, // открывать браузер при старте
+    browser: "chrome", // можно указать 'chrome', 'chrome.exe', 'google chrome', 'msedge', 'firefox' и т.д.
+  },
+});
