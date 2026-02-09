c80de5d04d2673d4fecea0a96fad59593027035e
Aleksei <boubnov28@gmail.com>
2026-02-08 20:05:51 -0600

    feat: add mobile hamburger menu

--- src/js/script.js
+++ src/js/script.js
@@ -8,6 +8,20 @@ import "purecss/build/grids-min.css";
 import "purecss/build/grids-responsive-min.css";
 import "/src/sass/style.scss";
 
+const burger = document.querySelector(".burger"),
+  close = document.querySelector(".header__menu-close"),
+  menu = document.querySelector(".header__menu");
+
+burger.addEventListener("click", () => {
+  menu.classList.add("header__menu_active");
+  document.body.style.overflow = "hidden";
+});
+
+close.addEventListener("click", () => {
+  menu.classList.remove("header__menu_active");
+  document.body.style.overflow = "";
+});
+
 const swiper = new Swiper(".swiper", {
   pagination: {
     el: ".swiper-pagination",
